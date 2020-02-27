fc4efd02ebf046daabff495f27bf842476f4a724
Marcin Biegun <marcin.biegun@swmansion.com>
2020-02-27 11:47:18 +0100

    Add moduledocs for native modules

--- lib/membrane_element_msdk_h264/encoder_sysmem_native.ex
+++ lib/membrane_element_msdk_h264/encoder_sysmem_native.ex
@@ -1,4 +1,8 @@
 defmodule Membrane.Element.Msdk.H264.EncoderSysmem.Native do
-  @moduledoc false
+  @moduledoc """
+  Native module responsible for calling the C++ encoder implementation.
+
+  Based on [https://github.com/Intel-Media-SDK/MediaSDK/tree/master/tutorials/simple_3_encode](https://github.com/Intel-Media-SDK/MediaSDK/tree/master/tutorials/simple_3_encode)
+  """
   use Unifex.Loader
 end
--- lib/membrane_element_msdk_h264/encoder_vmem_native.ex
+++ lib/membrane_element_msdk_h264/encoder_vmem_native.ex
@@ -1,4 +1,10 @@
 defmodule Membrane.Element.Msdk.H264.EncoderVmem.Native do
-  @moduledoc false
+  @moduledoc """
+  Native module responsible for calling the C++ encoder implementation.
+
+  Based on [https://github.com/Intel-Media-SDK/MediaSDK/tree/master/tutorials/simple_3_encode_vmem](https://github.com/Intel-Media-SDK/MediaSDK/tree/master/tutorials/simple_3_encode_vmem)
+
+  Warning! This implementation has an unsolved memory leak, see: [https://github.com/Intel-Media-SDK/MediaSDK/issues/1984](https://github.com/Intel-Media-SDK/MediaSDK/issues/1984)
+  """
   use Unifex.Loader
 end
